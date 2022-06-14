4047a7f1e196e9a7f40f711913bcdd8c38c2128f
Jarrod Bogard <jarrodbogard@gmail.com>
2022-06-14 15:23:07 -0500

    added static page

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const peopleRouters = require("./routers/people");
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
 app.use("/", peopleRouters);
+app.use(express.static('public'))
 
 app.listen(PORT, () => console.log(`I am listening on port ${PORT}`));
